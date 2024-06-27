4cbbd5e6b4bb27e05ce5eb75bd7a8ed58cd2446e
NoCode Consulting <connor@nocodeconsulting.com>
2024-06-27 13:02:25 -0500

    added card page back - next day

---
+++ app/card/page.tsx
@@ -0,0 +1,19 @@
+import Header from '@/components/Header';
+import ConnorGustafson from '@/components/ConnorGustafson';
+import Options from '@/components/Options';
+
+const Card = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <div className="flex-grow flex items-center justify-center">
+        <ConnorGustafson />
+      </div>
+      <div className="flex-grow flex items-center justify-center">
+        <Options />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
